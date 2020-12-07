93bbb6b3804d1f20da8eab5651521f6b0346c853
袁 仕超 <yuanshichao1985@gmail.com>
2020-12-07 20:38:22 +0800

    增加APP类统一管理各类管理类

---
+++ Code/StationDemo/APP.cs
@@ -0,0 +1,61 @@
+﻿using BaseDll;
+using CommonTools;
+using Communicate;
+using MotionIoLib;
+using System;
+using System.Windows.Forms;
+
+namespace StationDemo
+{
+    public class APP
+    {
+        #region 单例
+
+        private static readonly Lazy<APP> instance = new Lazy<APP>(() => new APP());
+
+        private APP()
+        {
+            MotionMgr = MotionMgr.GetInstace();
+            IOMgr = IOMgr.GetInstace();
+            AlarmMgr = AlarmMgr.GetIntance();
+            ConfigToolMgr = ConfigToolMgr.GetInstance();
+            StationMgr = StationMgr.GetInstance();
+            ParamSetMgr = ParamSetMgr.GetInstance();
+            ComMgr = ComMgr.GetInstance();
+            TcpMgr = TcpMgr.GetInstance();
+            SocketSeverMgr = SocketSeverMgr.GetInstace();
+        }
+
+        public static APP Instance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        #endregion 单例
+
+        #region 管理类
+        public MotionMgr MotionMgr;
+        public IOMgr IOMgr;
+        public AlarmMgr AlarmMgr;
+        public ConfigToolMgr ConfigToolMgr;
+        public StationMgr StationMgr;
+        public ParamSetMgr ParamSetMgr;
+        public ComMgr ComMgr;
+        public TcpMgr TcpMgr;
+        public SocketSeverMgr SocketSeverMgr;
+        #endregion
+
+        #region 界面  
+        public Form MainForm;
+        public Form AutoForm;
+        public Form Form_Set;
+        public Form Form_VisionDebug;
+        public Form Form_ParamSet;
+        public Form UserManger;
+        #endregion
+
+    }
+}
